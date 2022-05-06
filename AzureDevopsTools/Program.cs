d1c6816818315c3f699ffedd9b2891856e096abb
halis.saritas <halis.saritas@dstrace.com>
2022-05-06 17:29:46 +0300

    CreateIteration

--- AzureDevopsTools/Program.cs
+++ AzureDevopsTools/Program.cs
@@ -12,50 +12,60 @@ namespace AzureDevopsTools
 {
     internal class Program
     {
-        static string personalaccesstoken = "duev54spffquc66u7nazgt75zmx52seokpol2agdwks34wfcxaja";
-
+        static string PERSONAL_ACCESS_TOKEN = "duev54spffquc66u7nazgt75zmx52seokpol2agdwks34wfcxaja";
+        static string BASE_URL = "https://azure.dstrace.com/Yazilim";
+        
         static void Main(string[] args)
         {
-            //Console.WriteLine(CallAPIGet("https://azure.dstrace.com/Yazilim/DS%20GTS/_apis/build/builds?api-version=5.0"));
-            //Console.WriteLine(CallAPIGet("https://azure.dstrace.com/Yazilim/_apis/projects?api-version=5.0"));
-            //Console.WriteLine(CallAPIGet("https://azure.dstrace.com/Yazilim/DS%20OLT/_apis/work/teamsettings/iterations?api-version=6.0"));
-            //Console.WriteLine(CallAPIGet("https://azure.dstrace.com/Yazilim/DS%20OLT/_apis/wit/classificationnodes/?api-version=6.0"));
-            //Console.WriteLine(CallAPIGet("https://azure.dstrace.com/Yazilim/DS%20OLT/DS%20OLT%20Team/_apis/work/teamsettings/iterations?api-version=5.0"));
-            //var result = CallAPIDelete("https://azure.dstrace.com/Yazilim/DS%20OLT/_apis/wit/classificationnodes/iterations/Iteration%20z?api-version=5.0");            
-            //var result = CallAPIPost("https://azure.dstrace.com/Yazilim/DS%20OLT/DS%20OLT%20Team/_apis/work/teamsettings/iterations?api-version=5.0");
-            //Console.WriteLine(CallAPIGet("https://azure.dstrace.com/Yazilim/_apis/projects/DS%20OLT/teams?api-version=5.0"));
-            //CreateIteration("Iteration z", DateTime.Now.AddYears(1), DateTime.Now.AddYears(2));     
+            //Console.WriteLine(CallAPIGet($"{BASE_URL}/_apis/projects?api-version=5.0"));
+            //Console.WriteLine(CallAPIGet($"{BASE_URL}/DS%20OLT/_apis/work/teamsettings/iterations?api-version=6.0"));
+            //Console.WriteLine(CallAPIGet($"{BASE_URL}/DS%20OLT/_apis/wit/classificationnodes/?api-version=6.0"));
+            //Console.WriteLine(CallAPIGet($"{BASE_URL}/DS%20OLT/DS%20OLT%20Team/_apis/work/teamsettings/iterations?api-version=5.0"));
+            //Console.WriteLine(CallAPIGet($"{BASE_URL}/_apis/projects/DS OLT/teams?api-version=5.0"));
+            //CallAPIDelete($"{BASE_URL}/DS%20OLT/_apis/wit/classificationnodes/iterations/Iteration%20z?api-version=5.0");
+
+            //CreateIteration("Iteration z", DateTime.Now.AddYears(1), DateTime.Now.AddYears(2));
             ProjectList();
         }
 
-        static async void CreateIteration(string iterationName, DateTime stDate, DateTime endDate)
+        static async void CreateIteration(string iterationName, string projectName, string teamName, DateTime stDate, DateTime endDate)
         {
-            var result = CallAPIPost("https://azure.dstrace.com/Yazilim/DS%20OLT/_apis/wit/classificationnodes/iterations?api-version=5.0",
+            var result = CallAPIPost($"{BASE_URL}/{projectName}/_apis/wit/classificationnodes/iterations?api-version=5.0",
                 new { name = iterationName, attributes = new { startDate = stDate, finishDate = endDate } });
             var obj = JObject.Parse(result);
             var identifier = obj["identifier"].ToString();
-            result = CallAPIPost("https://azure.dstrace.com/Yazilim/DS%20OLT/DS%20OLT%20Team/_apis/work/teamsettings/iterations?api-version=5.0",
+            result = CallAPIPost($"{BASE_URL}/{projectName}/{teamName}/_apis/work/teamsettings/iterations?api-version=5.0",
                 new { id = identifier });
+        }
 
+        static async void DeleteIteration(string iterationName, string projectName)
+        {
+            CallAPIDelete($"{BASE_URL}/{projectName}/_apis/wit/classificationnodes/iterations/{iterationName}?api-version=5.0");
         }
 
         static async void ProjectList()
         {
-
             try
             {
-                Uri uri = new Uri("https://azure.dstrace.com/Yazilim");
-                VssBasicCredential credentials = new VssBasicCredential("", personalaccesstoken);
+                Uri uri = new Uri(BASE_URL);
+                VssBasicCredential credentials = new VssBasicCredential("", PERSONAL_ACCESS_TOKEN);
                 using ProjectHttpClient projectHttpClient = new ProjectHttpClient(uri, credentials);
                 IEnumerable<TeamProjectReference> projects = projectHttpClient.GetProjects().Result;
                 foreach (var pr in projects)
                 {
-                    var teams = CallAPIGet("https://azure.dstrace.com/Yazilim/_apis/projects/DS%20OLT/teams?api-version=5.0");
+
+                    var result = CallAPIGet($"{BASE_URL}/_apis/projects/{pr.Name}/teams?api-version=5.0");
+                    var obj = JObject.Parse(result);
+                    var arr = JArray.Parse(obj["value"].ToString());
+                    //DeleteIteration("Iteration x", pr.Name);
+                    foreach (var item in arr) 
+                    {
+                        //CreateIteration("Iteration x", pr.Name, item["name"].ToString(), DateTime.Now.AddYears(1), DateTime.Now.AddYears(2));
+                    }
                 }
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
@@ -70,7 +80,7 @@ namespace AzureDevopsTools
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
                     Convert.ToBase64String(
                         System.Text.ASCIIEncoding.ASCII.GetBytes(
-                            string.Format("{0}:{1}", "", personalaccesstoken))));
+                            string.Format("{0}:{1}", "", PERSONAL_ACCESS_TOKEN))));
 
                 using (HttpResponseMessage response = client.GetAsync(url).Result)
                 {
@@ -92,7 +102,7 @@ namespace AzureDevopsTools
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
                     Convert.ToBase64String(
                         System.Text.ASCIIEncoding.ASCII.GetBytes(
-                            string.Format("{0}:{1}", "", personalaccesstoken))));
+                            string.Format("{0}:{1}", "", PERSONAL_ACCESS_TOKEN))));
                 var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
                 using (HttpResponseMessage response = client.PostAsync(url, content).Result)
                 {
@@ -114,7 +124,7 @@ namespace AzureDevopsTools
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
                     Convert.ToBase64String(
                         System.Text.ASCIIEncoding.ASCII.GetBytes(
-                            string.Format("{0}:{1}", "", personalaccesstoken))));
+                            string.Format("{0}:{1}", "", PERSONAL_ACCESS_TOKEN))));
                 using (HttpResponseMessage response = client.DeleteAsync(url).Result)
                 {
                     var ensure = response.EnsureSuccessStatusCode();
